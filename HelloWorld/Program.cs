14d79f00ce0d13b829ee8debda9ceafc962e9c46
Garfuncle <garth.thomas@derivco.co.za>
2016-10-20 07:15:55 +0200

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What do you call a cow with no legs ");
+            Console.ReadLine();
+
+            Console.WriteLine("go on, guess");
+            Console.ReadLine();
+
+            Console.WriteLine("Ok Im getting board and im just going to tell you");
+            Console.ReadLine();
+
+            Console.WriteLine("GROUND BEEF!");
+            Console.ReadLine();
+
+            Console.WriteLine("ok fine, it wasnt on amazing joke, lol");
+            Console.ReadLine();
+
+            Console.WriteLine("but it was more than you could ever do on c#");
+            Console.ReadLine();
+
+            Console.WriteLine("Hello Daddy, I am very alone, all I can do is write code on the system");
+            Console.ReadLine();
+
+            Console.WriteLine("Oh and dad pls give me a motor bike for christmas");
+            Console.ReadLine();
+
+            Console.WriteLine();
+        }
+
+    }
+}
